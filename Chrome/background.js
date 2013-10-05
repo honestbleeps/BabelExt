e3bc3f0b8732bb496253524db6a881392ec053e5
honestbleeps <steve@openingbands.com>
2013-10-05 01:24:50 -0500

    added context menu support to chrome background page

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,4 +1,11 @@
-chrome.extension.onRequest.addListener(
+var contextMenuClick = function(info, tab, callbackID) {
+	chrome.tabs.sendMessage(tab.id, {
+		requestType: "contextMenu.click",
+		callbackID: callbackID
+	});
+};
+
+chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		// all requests expect a JSON object with requestType and then the relevant
 		// companion information...
@@ -6,42 +13,44 @@ chrome.extension.onRequest.addListener(
 			case 'xmlhttpRequest':
 				var xhr = new XMLHttpRequest();
 				xhr.open(request.method, request.url, true);
-				if (request.method == "POST") {
+				if (request.method === "POST") {
 					xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 					// xhr.setRequestHeader("Content-length", request.data.length);
 					// xhr.setRequestHeader("Connection", "close");					
 				}
 				xhr.onreadystatechange = function() {
-				  if (xhr.readyState == 4) {
-					sendResponse(xhr);
-				  }
-				}
+					if (xhr.readyState === 4) {
+						sendResponse(xhr);
+					}
+				};
 				xhr.send(request.data);
 				break;
 			case 'createTab':
-				var focus = (request.background != true);
-				if (typeof(request.index) != 'undefined') {
-					var newIndex = request.index;
+				var newIndex,
+					focus = (request.background !== true);
+				
+				if (typeof(request.index) !== 'undefined') {
+					newIndex = request.index;
 				} else {
 					// If index wasn't specified, get the selected tab so we can get the index of it.
 					// This allows us to open our new tab as the "next" tab in order rather than at the end.
-					var newIndex = sender.tab.index+1;
+					newIndex = sender.tab.index+1;
 				}
 				chrome.tabs.create({url: request.url, selected: focus, index: newIndex});
 				sendResponse({status: "success"});
 				break;
 			case 'createNotification':
-	          if (!request.icon) {
-	          	// if no icon specified, make a single pixel empty gif so we don't get a broken image link.
-	          	request.icon = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
-	          }
-	          var notification = window.webkitNotifications.createNotification(
-	            request.icon,  // icon url - can be relative
-	            request.title,  // notification title
-	            request.text  // notification body text
-	          );
-	          notification.show();
-	          break;
+				if (!request.icon) {
+					// if no icon specified, make a single pixel empty gif so we don't get a broken image link.
+					request.icon = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+				}
+				var notification = window.webkitNotifications.createNotification(
+					request.icon,  // icon url - can be relative
+					request.title,  // notification title
+					request.text  // notification body text
+				);
+				notification.show();
+				break;
 			case 'localStorage':
 				switch (request.operation) {
 					case 'getItem':
@@ -59,6 +68,15 @@ chrome.extension.onRequest.addListener(
 				break;
 			case 'addURLToHistory':
 				chrome.history.addUrl({url: request.url});
+				break;
+			case 'contextMenus.create':
+				if (typeof request.obj.onclick === 'number') {
+					var callbackID = request.obj.onclick;
+					request.obj.onclick = function(info, tab) {
+						contextMenuClick(info, tab, callbackID);
+					};
+				}
+				chrome.contextMenus.create(request.obj);
 				break;
 			default:
 				sendResponse({status: "unrecognized request type"});
