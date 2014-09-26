a4378971bafe863d290edf448c391bccde3337b2
Andrew Sayers <frontier-forums@pileofstuff.org>
2014-09-26 19:43:10 +0100

    Changes to browser-specific files based on recent build changes

--- Firefox/lib/settings.js
+++ Firefox/lib/settings.js
@@ -1,3 +1,3 @@
 exports.include = ["http://babelext.com/*","https://babelext.com/*"];
 exports.contentScriptWhen = "ready";
-exports.contentScriptFile = ["self.data.url('BabelExt.js')","self.data.url('extension.js')"];
+exports.contentScriptFile = ["BabelExt.js","extension.js"];
