7885a6db0d83d25b9d9c4b0304e2008a039b31ba
Andrew Sayers <frontier-forums@pileofstuff.org>
2015-02-14 01:15:57 +0000

    Moved configuration to conf/

--- script/build.sh
+++ script/build.sh
@@ -2,21 +2,21 @@
 
 if [ "$1" == "release" ]
 then
-    if grep -q chrome_info lib/local_settings.json && [ -z "$CHROME_PASSWORD" ]
+    if grep -q chrome_info conf/local_settings.json && [ -z "$CHROME_PASSWORD" ]
     then
         read -p 'Password for chrome.google.com: ' -s CHROME_PASSWORD
         echo
     fi
     export CHROME_PASSWORD
 
-    if grep -q amo_info lib/local_settings.json && [ -z "$AMO_PASSWORD" ]
+    if grep -q amo_info conf/local_settings.json && [ -z "$AMO_PASSWORD" ]
     then
         read -p 'Password for addons.mozilla.org: ' -s AMO_PASSWORD
         echo
     fi
     export AMO_PASSWORD
 
-    if grep -q opera_info lib/local_settings.json && [ -z "$OPERA_PASSWORD" ]
+    if grep -q opera_info conf/local_settings.json && [ -z "$OPERA_PASSWORD" ]
     then
         read -p 'Password for developer.opera.com: ' -s OPERA_PASSWORD
         echo
